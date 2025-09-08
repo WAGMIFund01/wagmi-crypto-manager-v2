f7a24590970d983a31f7094aeeeea4a7fbd3634a
Loye Oyawoye <loyeoyawoye@Loyes-MacBook-Pro.local>
2025-09-08 19:26:37 -0400

    Force deployment: Update home page to use new investor login form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { InvestorLoginForm } from '@/features/home/components/InvestorLoginForm';
 
 export default function Home() {
+  // Updated to use new investor login form
   return <InvestorLoginForm />;
 }
